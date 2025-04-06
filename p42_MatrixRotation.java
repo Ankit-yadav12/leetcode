e884a6b72f1e1dd3e814b593583f8aa33045af51
Ankit Yadav <py90398@gmail.com>
2025-04-06 21:36:33 +0530

    solved problem 48

---
+++ p42_MatrixRotation.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+
+public class p42_MatrixRotation {
+    public static void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // Transpose the matrix
+        for(int i = 0; i < n; i++) {
+            for(int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Reverse each row
+        for(int[] row : matrix) {
+            int start = 0, end = n - 1;
+            while(start < end) {
+                int temp = row[start];
+                row[start] = row[end];
+                row[end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("Original Matrix:");
+        for (int[] row : matrix) {
+            System.out.println(Arrays.toString(row));
+        }
+
+        rotate(matrix);
+
+        System.out.println("\nRotated Matrix:");
+        for (int[] row : matrix) {
+            System.out.println(Arrays.toString(row));
+        }
+    }
+}
